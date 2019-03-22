b5d88f27fabe20b33c391a18519762ddd20f49f8
OutrageousStop <ankitband@gmail.com>
2019-03-22 19:51:57 +0530

    live tracking

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import requests
 import json
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import wikipedia
+
+def fetch_data(key):
+    data = wikipedia.summary(key)
+    return data
 
 def astronauts_data():
     astronauts = requests.get('http://api.open-notify.org/astros.json')
@@ -14,28 +19,42 @@ def location_data():
     location = json.loads(location.text)
     return location
 
-def plot(lats, longs):
-    lats = float(lats)
-    longs = float(longs)
-    print('latitude: ', lats, 'longitude: ', longs)
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1,projection=ccrs.PlateCarree())
-    ax.scatter(longs, lats, c = '#FF0000')
-    ax.stock_img()
-    ax.coastlines()
-    ax.gridlines()
-    plt.show()
-
 def printAstronauts(astronauts):
     print("People Currently in ISS: ")
     for i in astronauts['people']:
         print(i['name'])
+def get_names(astronauts_data):
+    names = []
+    for i in astronauts_data['people']:
+        names.append(i['name'])
+    return names
 
-
+def liveTrack():
+    for i in range(20):
+        location = location_data()
+        longs = location['iss_position']['longitude']
+        lats = location['iss_position']['latitude']
+        lats = float(lats)
+        longs = float(longs)
+        print('latitude: ', lats, 'longitude: ', longs)
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1,projection=ccrs.PlateCarree())
+        ax.scatter(longs, lats, c = '#FF0000')
+        ax.stock_img()
+        ax.coastlines()
+        ax.gridlines()
+        plt.show(block= False)
+        plt.pause()
+        plt.close()
 if __name__ == "__main__":
     astronauts = astronauts_data()
+    names = get_names(astronauts)
+    details = {k:v for k,v in zip(names, [fetch_data(i) for i in names])}
+    for i in details:
+        print(i, details[i])
     location = location_data()
     longs = location['iss_position']['longitude']
     lats = location['iss_position']['latitude']
     printAstronauts(astronauts)
     plot(lats, longs)
+    liveTrack()
